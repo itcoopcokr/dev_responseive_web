ab088d8c35f6e331ba5b8338de78037989ac3e5f
itcoopcokr <itcoopcokr@gmail.com>
2017-11-28 17:28:14 +0900

    carousel

--- landingpage/js/custom.js
+++ landingpage/js/custom.js
@@ -1,5 +1,37 @@
+/*==============================
+        WOW
+================================*/
 $(function() {
     // Handler for .ready() called.
     new WOW().init();
 
 });
+/*==========================================
+        Magnific Popup
+http://dimsemenov.com/plugins/magnific-popup/
+===========================================*/
+$(function() {
+    // Handler for .ready() called.
+    $('#work').magnificPopup({
+      delegate: 'a', // child items selector, by clicking on it popup will open
+      type: 'image',
+      gallery:{
+          enabled: true
+      }
+      // other options
+    });
+
+});
+
+/*==========================================
+        Owl Carousel
+http://owlcarousel2.github.io/OwlCarousel2/index.html
+===========================================*/
+$(function() {
+    // Handler for .ready() called.
+    $(".owl-carousel").owlCarousel({
+        items : 3,
+    });
+
+});
+
